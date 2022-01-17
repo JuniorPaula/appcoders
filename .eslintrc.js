20808639f37a75edc42c64abf902f1ec77bd22fd
Junior Paula <slucivaldo509@gmail.com>
2022-01-17 20:16:23 -0300

    initial configurate of the project

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
