eac8c79c35bc43de652e90adc108eccfaf77d501
Junior Paula <slucivaldo509@gmail.com>
2022-01-17 21:17:14 -0300

    configurate typeorm and database connecton

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,11 @@
+import 'reflect-metadata';
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
