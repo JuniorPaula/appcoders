488048e35237fb8612d9e70e0c12ac08a17742fb
Junior Paula <slucivaldo509@gmail.com>
2022-01-18 20:22:41 -0300

    service to create inquilino

---
+++ src/modules/inquilinos/services/CreateInquilinosService.ts
@@ -0,0 +1,43 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import InquilinosRepository from '../repositories/InquilinosRepository';
+import Inquilino from '../typeorm/entities/Inquilino';
+
+interface IRequest {
+  nome: string;
+  idade: number;
+  sexo: string;
+  telefone: string;
+  email: string;
+}
+
+class CreateInquilinosService {
+  public async execute({
+    nome,
+    idade,
+    sexo,
+    telefone,
+    email,
+  }: IRequest): Promise<Inquilino> {
+    const inquilinosRepository = getCustomRepository(InquilinosRepository);
+    const emailExists = await inquilinosRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Email address already used.');
+    }
+
+    const inquilino = inquilinosRepository.create({
+      nome,
+      idade,
+      sexo,
+      telefone,
+      email,
+    });
+
+    await inquilinosRepository.save(inquilino);
+
+    return inquilino;
+  }
+}
+
+export default CreateInquilinosService;
