9d1fb93e80e5ee45e5ff9ebc5e892317961c6037
Junior Paula <slucivaldo509@gmail.com>
2022-01-19 20:30:33 -0300

    add unidadesRoutes on global routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import inquilinosRoutes from '@modules/inquilinos/routes/inquilinosRoutes';
+import unidadesRoutes from '@modules/unidades/routes/unidadesRoutes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/inquilinos', inquilinosRoutes);
+routes.use('/unidades', unidadesRoutes);
 
 export default routes;
