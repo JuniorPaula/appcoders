1d16a9007b8160bdb4fb271d6935470f8ee5569e
Junior Paula <slucivaldo509@gmail.com>
2022-01-19 20:44:01 -0300

    create UnidadesControllers

--- src/modules/unidades/controllers/UnidadesController.ts
+++ src/modules/unidades/controllers/UnidadesController.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
 import CreateUnidadesService from '../services/CreateUnidadesService';
+import ListUnidadesService from '../services/ListUnidadesService';
 
 export default class UnidadesController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUnidades = new ListUnidadesService();
+
+    const unidades = await listUnidades.execute();
+
+    return response.json(unidades);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { identificacao, proprietario, condominio, endereco } = request.body;
 
