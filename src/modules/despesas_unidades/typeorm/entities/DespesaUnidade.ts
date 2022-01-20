bdff2efde26d63f2c31b808d51b14253de25140e
Junior Paula <slucivaldo509@gmail.com>
2022-01-19 21:14:12 -0300

    create Entity DespesasUnidades

---
+++ src/modules/despesas_unidades/typeorm/entities/DespesaUnidade.ts
@@ -0,0 +1,46 @@
+import Unidade from '@modules/unidades/typeorm/entities/Unidade';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('despesas_unidades')
+class DespesaUnidade {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  descricao: string;
+
+  @Column()
+  tipo_despesa: string;
+
+  @Column('decimal')
+  valor: number;
+
+  @Column()
+  vencimento_fatura: Date;
+
+  @Column()
+  status_pagamento: boolean;
+
+  @Column()
+  unidade_id: string;
+
+  @ManyToOne(() => Unidade)
+  @JoinColumn({ name: 'unidade_id' })
+  unidade: Unidade;
+
+  @CreateDateColumn()
+  created_At: Date;
+
+  @UpdateDateColumn()
+  updated_At: Date;
+}
+
+export default DespesaUnidade;
